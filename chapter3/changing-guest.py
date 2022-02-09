dc22e619872c548e757e213d5d41f14f6291f858
asaldelkhosh <asaldelkhosh@gmail.com>
2022-02-09 17:09:47 +0330

    exercise5

---
+++ chapter3/changing-guest.py
@@ -0,0 +1,8 @@
+#exercise3-5
+
+guests = ['shakib', 'mah', 'sep', 'nafas', 'mina']
+
+not_come = guests.pop()
+print(not_come)
+guests.append('saba')
+print(f"Hi {guests[-1]}, I would like to invite u to dinner.")
