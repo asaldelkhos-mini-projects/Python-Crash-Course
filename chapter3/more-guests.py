38b8d6bc8b36f7c90c9e92c87cfed8283548f36c
asaldelkhosh <asaldelkhosh@gmail.com>
2022-02-11 20:28:54 +0330

    exercise6

---
+++ chapter3/more-guests.py
@@ -0,0 +1,12 @@
+#exercise3-6
+
+guests = ['shakib', 'mah', 'sep', 'nafas', 'saba']
+
+print(f"Hi {guests[0].title()}, I found a bigger table so i invite more friends.")
+
+guests.insert(0, 'amirhossein')
+guests.insert(3, 'sina')
+guests.insert(7, 'pouya')
+
+print(guests)
+print("Hi, i have a dinner night, I would like if ur comiing")
