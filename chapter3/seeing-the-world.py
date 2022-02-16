da1f4b7288604edec9595a61a2beb25ad955d381
asaldelkhosh <asaldelkhosh@gmail.com>
2022-02-16 21:52:13 +0330

    exercise8

---
+++ chapter3/seeing-the-world.py
@@ -0,0 +1,12 @@
+#exercise3-8
+
+countries = ['switzerland', 'paris', 'Italy', 'spain', 'turkey']
+print(countries)
+#ordered in alfabet
+print(sorted(countries))
+#orginal
+print(countries)
+#sorted in reverse
+print(sorted(countries, key = None, reverse = True))
+#orginal
+print(countries)
