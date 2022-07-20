4922147fdb9173dee377391de2a38e7e8fce0e5b
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-20 19:48:58 +0430

    an additional line out of for loop

--- chapter4/Pizzas.py
+++ chapter4/Pizzas.py
@@ -6,7 +6,10 @@ for pizza in pizzas_list:
     print(pizza)
 print("\n")
 
-#using for loop to print a sentense for each pizza
+#using for loop to print a sentence for each pizza
 
 for pizza in pizzas_list:
     print(f"I like {pizza.title()} pizza")
+print("\n")
+
+print(f"I really like eating diffrent kind of pizzas, so I try many diffrent one and my number one fav is {pizzas_list[0]}.\nI really love pizza!")
