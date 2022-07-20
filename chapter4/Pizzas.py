a3d7823ca5af238f773a20f70c886ea0c3d54df2
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-20 19:38:25 +0430

    using for loop, title() and list to print a sentence for each item in a list

--- chapter4/Pizzas.py
+++ chapter4/Pizzas.py
@@ -4,3 +4,9 @@ pizzas_list = ['pepperoni','napoletana', 'new york-style', 'quattro formaggi']
 
 for pizza in pizzas_list:
     print(pizza)
+print("\n")
+
+#using for loop to print a sentense for each pizza
+
+for pizza in pizzas_list:
+    print(f"I like {pizza.title()} pizza")
