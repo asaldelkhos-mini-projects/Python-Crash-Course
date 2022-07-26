8e7400959ad27a74ea3331fb131a41dda0fad324
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-26 19:55:12 +0430

    adding a exercise4-10 end of this code

--- chapter4/squares.py
+++ chapter4/squares.py
@@ -1,3 +1,9 @@
 #exercise from book example's
 squares = [value**2 for value in range(1, 11)]
 print(squares)
+
+#exercise 4-10
+
+print(f"Three first items in your list are:{squares[:3]}")
+print(f"Three items from the middle of the list are:{squares[4:7]}")
+print(f"Three last items in the list are:{squares[-3:]}")
