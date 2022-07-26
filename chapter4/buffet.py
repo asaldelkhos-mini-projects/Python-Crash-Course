ea9e9943351593ad4a09aeba19e53d628bf06661
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-26 21:34:14 +0430

    what is tuple

---
+++ chapter4/buffet.py
@@ -0,0 +1,8 @@
+menu = ('burger', 'salad', 'water', 'pasta')
+for food in menu:
+    print(food)
+#menu[0] = 'pizza'  | this is not possible bc of the tuple
+
+menu = ('lazania', 'salad', 'juice', 'pasta')
+for food in menu:
+    print(food)
