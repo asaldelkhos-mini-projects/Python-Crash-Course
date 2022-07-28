7d4bfb4d5cde2916c1ed487eabfb14b8cb5293fd
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-28 20:05:41 +0430

    adding another condition

--- chapter5/admin.py
+++ chapter5/admin.py
@@ -2,8 +2,12 @@
 
 names = ['admin', 'sahar', 'ehsan', 'nima', 'nazi']
 
-for name in names:
-    if name == 'admin':
-        print('Hello admin, would you like to see a status report?')
-    else:
-        print(f'\nHello {name.title()}, thank you for logging again.')
+#if we didn't have any user then first massage would print otherwaise second and third one prints.
+if names == []:
+    print('We need to find some users!')
+else:
+    for name in names:
+        if name == 'admin':
+            print('Hello admin, would you like to see a status report?')
+        else:
+            print(f'\nHello {name.title()}, thank you for logging again.')
