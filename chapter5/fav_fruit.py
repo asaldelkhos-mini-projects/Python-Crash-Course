3a6a7a5094f2f19a12160ca7dd9e3a1b4457865b
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-28 18:28:48 +0430

    showing how if works

---
+++ chapter5/fav_fruit.py
@@ -0,0 +1,14 @@
+#exercize 5-7
+
+fav_fruits = ['banana', 'peach', 'mango', 'strawberry']
+
+if 'banana' in fav_fruits:
+    print('u really like banana')
+if 'watermelon' in fav_fruits:
+    print('u really like watermelon')
+if 'peach' in fav_fruits:
+    print('u really like peach')
+if 'mango' in fav_fruits:
+    print('u really like mango')
+if 'bluberry' in fav_fruits:
+        print('u really like bluberry')
