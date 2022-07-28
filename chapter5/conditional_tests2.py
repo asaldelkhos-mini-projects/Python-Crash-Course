0efd95da6a5409e8d6571c740fea91cce68e0c71
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-28 15:15:36 +0430

    and, or, boolean, if

---
+++ chapter5/conditional_tests2.py
@@ -0,0 +1,14 @@
+#exercise from chapter5
+
+my_age = 21
+his_age = 20
+
+if my_age >= 21 and his_age >= 20:
+    print('Asal & Amir')
+
+if my_age == 21 or his_age < 20:
+    print('they are not Asal & AmiR')
+
+print(my_age == 20 and his_age == 21)
+print(my_age == 21 or his_age == 21)
+print(my_age >= his_age)
