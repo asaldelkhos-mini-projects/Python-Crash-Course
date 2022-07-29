242a6c476df60bd22a60bfa0a6198404f79f316f
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-29 16:04:41 +0430

    dictionaries

---
+++ chapter6/person.py
@@ -0,0 +1,13 @@
+#exercise 6-1 |dictionary
+
+person = {
+    'first_name' : 'Amir',
+    'last_name' : 'mine',
+    'age' : 21,
+    'city' : 'mashhad'
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])    
