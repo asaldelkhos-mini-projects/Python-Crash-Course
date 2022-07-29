8d0b3b6729c94611f9b978b9a846966d7df71383
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-29 13:16:34 +0430

    find the solutiongit add .

--- chapter5/usernames.py
+++ chapter5/usernames.py
@@ -2,10 +2,10 @@
 
 current_users = ['dua', 'ariana', 'selena', 'tylor', 'gaga']
 
-new_users = ['harry', 'zayn', 'the weekend', 'selena', 'gaga']
-for value in range(0, 5):
-    for user in new_users:
-        if user.title() == current_users[value].title():
-            print('please Enter a new Username, this one is already exist.')
-        else:
-            print('The username is avialable.')
+new_users = ['harry', 'zayn', 'the weekend', 'seleNa', 'GaGa']
+
+for name in new_users:
+    if name.lower() in current_users[:]:
+        print(f'please Enter a new Username, {name} is already exist.')
+    else:
+        print(f'{name} is avialable.')
