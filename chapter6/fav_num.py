7083a4efc738b068dbd4a46a4b0d294b63c7fd3d
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-29 16:26:11 +0430

    accessing values in dictionary

---
+++ chapter6/fav_num.py
@@ -0,0 +1,20 @@
+#exercise 6-2
+
+fav_num = {
+    'sahar' : 3,
+    'arash' : 7,
+    'poneh' : 99,
+    'parviz' : 85,
+    'poot' : 2
+}
+
+name = fav_num['sahar']
+print(f'sahar favorite number is {name}\n')
+name = fav_num['arash']
+print(f'arash favorite number is {name}\n')
+name = fav_num['poneh']
+print(f'poneh favorite number is {name}\n')
+name = fav_num['parviz']
+print(f'parviz favorite number is {name}\n')
+name = fav_num['poot']
+print(f'poot favorite number is {name}')
