8ba8848eeba7529ab1575dc4cce6422f64401996
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-30 22:07:46 +0430

    showig how to access through dictionaries by for loop

--- chapter6/person.py
+++ chapter6/person.py
@@ -7,5 +7,16 @@ person = {
     'city' : 'mashhad'
     }
 
+# access to values
 for info in person.values():
+    print(info)
+
+# access to everything in a dictionary
+for info, value in person.items():
+    print(f"\n{info}:")
+    print(value)
+print("\n")
+
+#access to keys
+for info in person.keys():
     print(info)
