2f2a423519289096d5d61e698dbfb10ad9ea53d4
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-06 21:02:06 +0430

    infinite code

--- chapter7/sandwich_order.py
+++ chapter7/sandwich_order.py
@@ -1,1 +1,16 @@
 # exercixe 7-8
+
+sandwich_orders = []
+finished_orders = []
+
+active = True
+while active:
+    order = input("Please enter your order:\n(When you are finish enter 'quit'.)")
+
+    if order == 'quit':
+        active == False
+    else:
+        sandwich_orders.append(order)
+        finished_orders.append(order)
+
+print("your order is ready.")
