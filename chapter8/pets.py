189e8ca7295e9082b30c6d3f4d3c74f98f788def
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-08 22:39:00 +0430

    now i have a new problem!

--- chapter8/pets.py
+++ chapter8/pets.py
@@ -1,8 +1,8 @@
 # testing sth for myself
 
-def pet_info(type ='dog', name):
+def pet_info(name, type ='dog'):
     print(f"I have a {type}")
     print(f"my {type}'s name is {name}")
 
-name = input(f"What is your {type}'s name? ")
-pet_info(type, name)
+name_pet = input("What is your dog's name? ")
+pet_info(type, name_pet)
