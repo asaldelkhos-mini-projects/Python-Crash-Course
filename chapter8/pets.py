c3fb557f6a8baf27b956099dfcea12942292ce34
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-09 00:18:30 +0430

    testing sth (chang a parameter)

--- chapter8/pets.py
+++ chapter8/pets.py
@@ -4,5 +4,7 @@ def pet_info(name, type ='dog'):
     print(f"\nI have a {type}")
     print(f"my {type}'s name is {name}")
 
-name_pet = input("What is your dog's name? ")
-pet_info(name_pet)
+name = input("What is your dog's name? ")
+pet_info(name)
+
+pet_info(name, type='cat')
