50f6e399685546e35f402d955487a5e7e75345a1
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-08 21:20:36 +0430

    testing sth and I getting error

---
+++ chapter8/pets.py
@@ -0,0 +1,8 @@
+# testing sth for myself
+
+def pet_info(type ='dog', name):
+    print(f"I have a {type}")
+    print(f"my {type}'s name is {name}")
+
+name = input(f"What is your {type}'s name? ")
+pet_info(type, name)
