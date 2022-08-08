4be4672f6c84981cd4a3efc1874ba170955fb508
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-08 22:47:30 +0430

    problem solved

--- chapter8/pets.py
+++ chapter8/pets.py
@@ -1,8 +1,8 @@
 # testing sth for myself
 
 def pet_info(name, type ='dog'):
-    print(f"I have a {type}")
+    print(f"\nI have a {type}")
     print(f"my {type}'s name is {name}")
 
 name_pet = input("What is your dog's name? ")
-pet_info(type, name_pet)
+pet_info(name_pet)
