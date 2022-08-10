93626e148b8373c83f299660f1651d403759a1b8
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-10 23:24:13 +0430

    finally it is working well

--- chapter8/user_profile.py
+++ chapter8/user_profile.py
@@ -1,6 +1,6 @@
 def build_profile(first, last, **user_info):
     user_info['firs_name'] = first
-    user_info['last_name'] = last_name
+    user_info['last_name'] = last
     return user_info
 
 user_profile = build_profile('Asal', 'Delkhosh',
