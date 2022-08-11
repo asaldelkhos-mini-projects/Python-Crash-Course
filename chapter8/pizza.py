72618fbc07405147b810745d6417073dffd66801
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-11 20:13:43 +0430

    it is how to import an entire module

--- chapter8/pizza.py
+++ chapter8/pizza.py
@@ -1,5 +1,5 @@
 # just function
-def make_pizza(size, **toppings):
+def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
