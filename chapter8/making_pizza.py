c42a868f101d62b91a6edec3dbbdd7cbb9c59927
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-11 20:24:56 +0430

    importing specific functions

--- chapter8/making_pizza.py
+++ chapter8/making_pizza.py
@@ -1,4 +1,4 @@
-import pizza
+from pizza import make_pizza
 
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
