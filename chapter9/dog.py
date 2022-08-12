aa448a9690ec3a37ff705ed8ff468b69ec35a1e3
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-12 22:38:45 +0430

    buggit  add .

--- chapter9/dog.py
+++ chapter9/dog.py
@@ -13,3 +13,9 @@ class Dog:
     def roll_over(self):
         """simulate a dog rolling over in response to a commad."""
         print(f"{self.name} is rolled over!")
+
+dog_name = input("What is your dog name? ")
+dog_age = input("How old is your dog? ")
+Dog(dog_name,dog_age)
+dog_name.sit()
+dog_name.roll_over()
