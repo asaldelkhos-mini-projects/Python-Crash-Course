91ff763547412e463df0de0323459229b772ae1c
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-13 18:49:04 +0430

    make the code shorter and adding a defalt value for an attribute

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -6,14 +6,13 @@ class Restaurant:
         """initialize name and age cuisine"""
         self.name = name
         self.cuisine = cuisine
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"Restaurant name is {self.name}.")
         print(f"Restaurant cuisine type is {self.cuisine}")
-
-    def open_restaurant(self):
         print(f"{self.name} is open.")
+        print(f"{self.number_served}")
 
 restaurant = Restaurant('Rezae', 'Iranian food')
 restaurant.describe_restaurant()
-restaurant.open_restaurant()
