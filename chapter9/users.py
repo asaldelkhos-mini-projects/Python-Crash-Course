d77b6d83655eef460643cafd29d6cd1f3bb66e0b
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-14 21:40:03 +0430

    tryig to find out what is wrong

--- chapter9/users.py
+++ chapter9/users.py
@@ -5,7 +5,7 @@ class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.login_attempts = 0
+    #    self.login_attempts = 0
 
     def describe_user(self):
         print(f"My name is {self.first_name} {self.last_name}")
@@ -13,8 +13,8 @@ class User:
     def greet_user(self):
         print(f"Hi dear {self.first_name}")
 
-    def  inccrement_login_attempts(self, num):
-        self.login_attempts += num
+#    def  inccrement_login_attempts(self):
+#        self.login_attempts += self.login_attempts
 
 person1 = User('Asal', 'delkhosh')
 person1.describe_user()
