8756c1231181c768d27dad29bde1aa471c0982a1
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-15 18:11:26 +0430

    make a child class and get bug

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -17,6 +17,21 @@ class Restaurant:
     def update_number_served(self, num):
         self.number_served = num
 
+class IceecreamStand(Restaurant):
+        def __init__(self, name, cuisine):
+            """initialize attributes of parent class"""
+            super.__init__(name, cuisine)
+            self.flavors = Flavors_list()
+
+        def Flavors_list(self):
+            flavors = ['chocolate', 'vanila', 'coffee', 'stawberry']
+            for flavor in flavors:
+                print(flavor)
+
+restaurant = IceecreamStand('shad ice cream', 'ice cream')
+restaurant.Flavors_list()                
+"""
 restaurant = Restaurant('Rezae', 'Iranian food')
 restaurant.update_number_served(10)
 restaurant.describe_restaurant()
+"""
