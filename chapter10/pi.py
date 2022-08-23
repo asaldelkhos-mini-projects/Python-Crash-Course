8143a2a3bb59b7046122c9793bef95dfb3afa5ab
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-23 19:33:14 +0430

    We want to check if our birthday appears in pi or not

--- chapter10/pi.py
+++ chapter10/pi.py
@@ -6,3 +6,9 @@ with open(filename) as file_object:
 pi_string = ''
 for line in lines:
     pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appears in the first million digits of pi.")
