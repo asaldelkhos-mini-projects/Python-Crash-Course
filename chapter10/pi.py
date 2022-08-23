865aca9eda88d0e2b7139ea4145c924b5da88778
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-23 19:25:22 +0430

    make a list from file in line 4 and then turn that to a string

--- chapter10/pi.py
+++ chapter10/pi.py
@@ -1,3 +1,7 @@
 filename = 'pi_million_digits.txt'
 
-with open(filename) as file_object.readlines()
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    pi_string += line.strip()    
