ea6a1ed4c15c27b7c3de2352902b6b21873c5f28
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-24 21:30:33 +0430

    buggg

---
+++ chapter10/guest.py
@@ -0,0 +1,12 @@
+failname = 'guest.txt'
+
+with open(filename, 'a') as file_object:
+    guest_name = []
+    active = True
+    while active:
+        name = input("What is your name? ")
+        if name == Finish:
+            active = False
+        else:
+            guest_name.append(name)
+    file_object.write(guest_name)
