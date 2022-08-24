c50daea545be80b3f141a51920d672203f7c4022
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-24 20:15:32 +0430

    appendng without erased

--- chapter10/write_message2.py
+++ chapter10/write_message2.py
@@ -1,5 +1,4 @@
 filename = 'programming.txt'
 
-with open(filename, 'w') as file_object:
+with open(filename, 'a') as file_object:
     file_object.write("I also love finding meaning in large datasets.\n")
-    file_object.write("I love creating new games.\n")
