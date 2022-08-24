ae26ba974242c82fc2447001b5c4b2bdab176ec1
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-24 20:09:24 +0430

    teting for myselfe

---
+++ chapter10/write_message2.py
@@ -0,0 +1,5 @@
+filename = 'programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating new games.\n")
