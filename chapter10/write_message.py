e8349498f72f8326e5c4da03aa6e8896bdaa55a5
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-24 19:43:20 +0430

    make it look better

--- chapter10/write_message.py
+++ chapter10/write_message.py
@@ -1,6 +1,5 @@
-filename = 'programmig.txt'
+filename = 'programming.txt'
 
 with open(filename, 'w') as file_object:
-    file_object.write("I love programming")
-    file_object.write("I love creating new games.")
-    
+    file_object.write("I love programming\n")
+    file_object.write("I love creating new games.\n")
