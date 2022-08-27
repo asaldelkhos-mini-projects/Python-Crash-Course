397ff90aedd1df07d1a91b9ee7532d2e2907e840
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-27 13:50:01 +0430

    get a bug

--- chapter10/addition.py
+++ chapter10/addition.py
@@ -2,5 +2,11 @@ print("Give me two number, and I'll add them together")
 print("Enter 'q' to quit")
 
 first_number = input("First number: ")
+if first_number == 'q':
+    break
 second_number = input("Second number: ")
+if second_number == 'q':
+    break
+
 sum = int(first_number) + int(second_number)
+print(sum)
