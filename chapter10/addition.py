98497e658b1292b63dde167636144c767648ba65
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-27 14:10:01 +0430

    working well, now adding exception

--- chapter10/addition.py
+++ chapter10/addition.py
@@ -1,12 +1,13 @@
 print("Give me two number, and I'll add them together")
 print("Enter 'q' to quit")
 
-first_number = input("First number: ")
-if first_number == 'q':
-    break
-second_number = input("Second number: ")
-if second_number == 'q':
-    break
+while True:
+    first_number = input("First number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
 
-sum = int(first_number) + int(second_number)
-print(sum)
+    sum = int(first_number) + int(second_number)
+    print(sum)
