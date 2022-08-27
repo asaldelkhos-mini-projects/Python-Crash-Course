46d31b71c64a10c58130d3391e3b0dcc65acf200
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-27 13:17:05 +0430

    let's add if statement for quitting

---
+++ chapter10/addition.py
@@ -0,0 +1,6 @@
+print("Give me two number, and I'll add them together")
+print("Enter 'q' to quit")
+
+first_number = input("First number: ")
+second_number = input("Second number: ")
+sum = int(first_number) + int(second_number)
