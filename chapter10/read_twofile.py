61b0576967ff6ed638679bc67b5b67334c753550
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-27 14:44:55 +0430

    at first we wrote a function to reads and prints

---
+++ chapter10/read_twofile.py
@@ -0,0 +1,8 @@
+def read(filename):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} doesn't exist.")
+    else:
+        print(contents)            
