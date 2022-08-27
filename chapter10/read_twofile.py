b17c19c1f3ba85a3bbf162058ca6cbcfe12a91c2
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-27 14:51:29 +0430

    now call our files

--- chapter10/read_twofile.py
+++ chapter10/read_twofile.py
@@ -1,8 +1,12 @@
-def read(filename):
+def read_file(filename):
     try:
         with open(filename, encoding='utf-8') as f:
             contents = f.read()
     except FileNotFoundError:
         print(f"Sorry, the file {filename} doesn't exist.")
     else:
-        print(contents)            
+        print(contents)
+
+filenames = ['dogs.txt', 'cats.txt']
+for filename in filenames:
+    read_file(filename)
