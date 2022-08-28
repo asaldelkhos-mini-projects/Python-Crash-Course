6ca04579f604221d5eeff026b2c17f2ebd33b535
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-28 19:44:31 +0430

    Prompt for new username

---
+++ chapter10/remember_me.py
@@ -0,0 +1,12 @@
+import json
+
+# Get stored username if available
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
