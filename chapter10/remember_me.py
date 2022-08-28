9b5738186d526688c1380b5720398c8260f0e55f
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-28 20:03:49 +0430

    call the function, and check if the program working well

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -26,4 +26,6 @@ def greet_user():
         print(f"Welcome back, {username}!")
     else:
         username = get_new_username()
-        print(f"We'll remember you when you come back, {username}")    
+        print(f"We'll remember you when you come back, {username}")
+
+greet_user()
