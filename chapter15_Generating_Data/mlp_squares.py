3b014a9ca3ca2fc84abc1352241e48a582f4aef1
asaldelkhosh <asaldelkhosh@gmail.com>
2022-10-11 10:00:56 +0330

    line 5 and the discription from 7 to 13

--- chapter15_Generating_Data/mlp_squares.py
+++ chapter15_Generating_Data/mlp_squares.py
@@ -1,3 +1,13 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+
+fig, ax = plt.subplots()
+"""
+-subplots() can generate one or more
+plots in the same figure
+-fig represents the entire figure or collection
+of plots that are generated
+-ax represents a single plot in the figure
+
+"""
